cd7d2fb06e5a5df412fe13d7bc0e408358ca9809
Squiddyinkster :) ¯\_(ツ)_/¯  ( <almighty.alexanderz@gmail.com>
2026-04-13 16:40:04 -0400

    Create  I-m-a-math-Jedi-.py

---
+++ I-m-a-math-Jedi-.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+t.write("I'M A MATH JEDI")
+t.right(90)
+t.forward(2)
+t.left(90)
+t.forward(100)
+t.penup()
+t.right(90)
+t.forward(10)
+t.right(90)
+t.shape("turtle")
+t.forward(80)
+t.color("blue")
+#simple
